d7137f37f5c3ab5fec4cc75e8d965293aca9789f
Dennis Kirsch <dennis.kirsch@chainofalliance.com>
2022-05-11 12:50:17 +0200

    Optimize PostchainResponse

--- Assets/postchain-unity-async/Runtime/Chromia/PostchainClient/PostchainResponse.cs
+++ Assets/postchain-unity-async/Runtime/Chromia/PostchainClient/PostchainResponse.cs
@@ -1,5 +1,6 @@
 using UnityEngine.Networking;
 using Newtonsoft.Json;
+using System;
 
 namespace Chromia.Postchain.Client
 {
@@ -7,12 +8,29 @@ namespace Chromia.Postchain.Client
     {
         public bool Error => _error;
         public string ErrorMessage => _errorMessage;
-        public T Content => JsonConvert.DeserializeObject<T>(_rawContent);
         public string RawContent => _rawContent;
+        public T Content
+        {
+            get
+            {
+                if (default(T) == null || default(T).Equals(_content))
+                {
+                    if (String.IsNullOrEmpty(_rawContent))
+                    {
+                        return default(T);
+                    }
+
+                    _content = JsonConvert.DeserializeObject<T>(_rawContent);
+                }
+
+                return _content;
+            }
+        }
 
         private bool _error;
         private string _errorMessage;
         private string _rawContent;
+        private T _content;
 
         public static PostchainResponse<T> SuccessResponse()
         {
@@ -20,7 +38,8 @@ namespace Chromia.Postchain.Client
             {
                 _error = false,
                 _errorMessage = null,
-                _rawContent = null
+                _rawContent = null,
+                _content = default(T)
             };
         }
 
@@ -30,7 +49,8 @@ namespace Chromia.Postchain.Client
             {
                 _error = false,
                 _errorMessage = null,
-                _rawContent = JsonConvert.SerializeObject(content)
+                _rawContent = null,
+                _content = content
             };
         }
 
@@ -40,7 +60,8 @@ namespace Chromia.Postchain.Client
             {
                 _error = true,
                 _errorMessage = errorMessage,
-                _rawContent = null
+                _rawContent = null,
+                _content = default(T)
             };
         }
 
